1e875148b7f94b7a78be5a4e30549cd2d28c6bc9
Muhammad <Muhammad@192.168.1.104>
2015-03-26 01:58:54 +0200

    DistinctList

---
+++ DistinctList.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * Using the PHP language, have the function DistinctList(arr) take the array of numbers 
+ * stored in arr and determine the total number of duplicate entries. 
+ * For example if the input is [1, 2, 2, 2, 3] then your program should output 2 because there are two 
+ * duplicates of one of the elements. 
+ * 
+ * Input = 0,-2,-2,5,5,5 Output = 3
+ * Input = 100,2,101,4 Output = 0
+ */
+
+function DistinctList($arr) {  
+    $max = max(array_count_values($arr));
+    if($max == 1)
+        return 0;
+    return $max-1; 
+         
+}
+   
+echo DistinctList(fgets(fopen('php://stdin', 'r')));  
